8b8bc871b87618ff8f1ecb71e28d7fbd5882dc5e
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-18 02:41:01 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,11 @@
-require 'pry'
 def my_collect(names)
   i = 0
   new_array = []
   while i < names.length
   new_array << yield(names[i])
-  i += 0
+  i += 1
 end
 new_array
 end
-binding.pry
+
 my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) {|name| name.split(" ").first}
